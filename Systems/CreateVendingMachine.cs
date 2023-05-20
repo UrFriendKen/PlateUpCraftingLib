778510bc0520bfa07b09583616c957342cbb2d77
UrFriendKen <51278666+UrFriendKen@users.noreply.github.com>
2023-05-20 10:11:02 +0800

    Moved populating vending machine to another system, to allow for refresh

--- Systems/CreateVendingMachine.cs
+++ Systems/CreateVendingMachine.cs
@@ -5,7 +5,6 @@ using KitchenData;
 using KitchenLib.Utils;
 using KitchenMods;
 using System.Collections.Generic;
-using System.Linq;
 using Unity.Entities;
 using UnityEngine;
 
@@ -36,19 +35,7 @@ namespace CraftingLib.Systems
                 int num = ((!(frontDoor.x > 0f)) ? 1 : (-1));
                 ctx.Set(entity, new CPosition(frontDoor + new Vector3(num * 2, 0f, 0f)));
                 ctx.Set(entity, new CDoNotPersist());
-                IEnumerable<AppliancePart> parts = GameData.Main.Get<AppliancePart>();
-                DynamicBuffer<CVendorOption> options = ctx.AddBuffer<CVendorOption>(entity);
-                foreach (AppliancePart appliancePart in GameData.Main.Get<AppliancePart>())
-                {
-                    if (appliancePart.IsPurchasable)
-                    {
-                        options.Add(new CVendorOption()
-                        {
-                            ID = appliancePart.ID,
-                            PurchaseCost = appliancePart.PurchaseCost
-                        });
-                    }
-                }
+                ctx.Set(entity, new RefreshVendingMachineOptions.SRefreshOptions());
             }
         }
     }
---
+++ Systems/RefreshVendingMachineOptions.cs
@@ -0,0 +1,59 @@
+﻿using CraftingLib.GameDataObjects;
+using Kitchen;
+using KitchenData;
+using KitchenMods;
+using System.Collections.Generic;
+using Unity.Entities;
+
+namespace CraftingLib.Systems
+{
+    public class RefreshVendingMachineOptions : NightSystem, IModSystem
+    {
+        public struct SRefreshOptions : IComponentData, IModComponent { }
+
+        private static RefreshVendingMachineOptions _instance;
+
+        protected override void Initialise()
+        {
+            base.Initialise();
+            _instance = this;
+        }
+
+        protected override void OnUpdate()
+        {
+            if (!TryGetSingletonEntity<CreateVendingMachine.SVendingMachine>(out Entity singletonEntity))
+                return;
+
+            EntityContext ctx = new EntityContext(EntityManager);
+            if (Has<SRefreshOptions>())
+            {
+                IEnumerable<AppliancePart> parts = GameData.Main.Get<AppliancePart>();
+                if (!RequireBuffer<CVendorOption>(singletonEntity, out DynamicBuffer<CVendorOption> options))
+                {
+                    options = ctx.AddBuffer<CVendorOption>(singletonEntity);
+                }
+                options.Clear();
+                foreach (AppliancePart appliancePart in parts)
+                {
+                    if (appliancePart.IsPurchasable) // To add check for requirements (Possible attachment points/partial appliances exist?)
+                    {
+                        options.Add(new CVendorOption()
+                        {
+                            ID = appliancePart.ID,
+                            PurchaseCost = appliancePart.PurchaseCost
+                        });
+                    }
+                }
+            }
+        }
+
+        public static void RequestRefresh()
+        {
+            Entity singletonEntity = default;
+            bool? found = _instance?.TryGetSingletonEntity<CreateVendingMachine.SVendingMachine>(out singletonEntity);
+            if (!found.HasValue || !found.Value)
+                return;
+            _instance.Set<SRefreshOptions>(singletonEntity);
+        }
+    }
+}
